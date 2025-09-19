299f7751b0af4b17300f4166b6f1a3fa3046bac6
Carlos Sánchez <casc84ab@gmail.com>
2025-09-19 08:50:28 +0200

    Add imports for foundational commands in RegisterBusinessCommand

--- customer-domain-people-core/src/main/java/com/firefly/domain/people/core/business/commands/RegisterBusinessCommand.java
+++ customer-domain-people-core/src/main/java/com/firefly/domain/people/core/business/commands/RegisterBusinessCommand.java
@@ -1,5 +1,16 @@
 package com.firefly.domain.people.core.business.commands;
 
+import com.firefly.domain.people.core.party.commands.RegisterPartyCommand;
+import com.firefly.domain.people.core.status.commands.RegisterPartyStatusEntryCommand;
+import com.firefly.domain.people.core.contact.commands.RegisterIdentityDocumentCommand;
+import com.firefly.domain.people.core.contact.commands.RegisterAddressCommand;
+import com.firefly.domain.people.core.contact.commands.RegisterEmailCommand;
+import com.firefly.domain.people.core.contact.commands.RegisterPhoneCommand;
+import com.firefly.domain.people.core.compliance.commands.RegisterEconomicActivityLinkCommand;
+import com.firefly.domain.people.core.party.commands.RegisterPartyProviderCommand;
+import com.firefly.domain.people.core.party.commands.RegisterPartyRelationshipCommand;
+import com.firefly.domain.people.core.party.commands.RegisterPartyGroupMembershipCommand;
+
 import java.util.List;
 
 
